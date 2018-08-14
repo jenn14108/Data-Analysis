56d435e25059b4e6554ce85d8ac614f80e1e522e
jenn14108 <jenn.lee14108@gmail.com>
2018-08-14 15:29:47 -0400

    Change deprecated ix command to loc

---
+++ stocker/runStocker.py
@@ -0,0 +1,10 @@
+'''
+Followed instructions laid out by William Koehresen in the following two articles:
+# https://towardsdatascience.com/stock-analysis-in-python-a0054e2c1a4c
+# https://towardsdatascience.com/stock-prediction-in-python-b66555171a2
+run file with command pythonw runStocker.py
+'''
+from stocker import Stocker
+
+microsoft = Stocker('MSFT')
+print(microsoft);
